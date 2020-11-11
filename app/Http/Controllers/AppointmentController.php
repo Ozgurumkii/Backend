1f3e4ba6818bdac9f9097c73f82557d3f3287ff1
Özgür <ozgurumkii@gmail.com>
2020-11-11 18:24:39 +0300

    Necessary checks and crud processes regarding the appointment dates were prepared.
    Necessary checks and crud processes regarding the appointment dates were prepared.

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -45,6 +45,39 @@ class AppointmentController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            "customerid" => "required",
+            "apartmentid" => "required",
+            "startdate" => "required",
+            "enddate" => "required",
+        ]);
+
+        $appointment = new Appointment();
+        $appointment->customerid = $request->customerid;
+        $appointment->apartmentid = $request->apartmentid;
+        $appointment->startdate = $request->startdate;
+        $appointment->enddate = $request->enddate;
+
+        $appointmentDateControl = self::appointmentDateControl($request);
+        if($appointmentDateControl){
+            return response()->json([
+                "status" => false,
+                "message" => "Belirtilen randevu tarihi müsait değildir..."
+            ], 500);
+        }
+        else{
+            if ($this->user->appointments()->save($appointment)) {
+                return response()->json([
+                    "status" => true,
+                    "appointment" => $appointment
+                ]);
+            } else {
+                return response()->json([
+                    "status" => false,
+                    "message" => "Ops, task could not be saved."
+                ], 500);
+            }
+        }
     }
 
     /**
@@ -78,6 +111,38 @@ class AppointmentController extends Controller
      */
     public function update(Request $request, Appointment $appointment)
     {
+        $this->validate($request, [
+            "apartmentid" => "required",
+            "startdate" => "required",
+            "enddate" => "required",
+        ]);
+
+        $appointment->apartmentid = $request->apartmentid;
+        $appointment->startdate = $request->startdate;
+        $appointment->enddate = $request->enddate;
+
+        $appointmentDateControl = self::appointmentDateControl($request);
+        if($appointmentDateControl)
+        {
+            return response()->json([
+                "status" => false,
+                "message" => "Belirtilen randevu tarihi müsait değildir..."
+            ], 500);
+        }
+        else
+        {
+            if ($this->user->appointments()->save($appointment)) {
+                return response()->json([
+                    "status" => true,
+                    "appointment" => $appointment
+                ]);
+            } else {
+                return response()->json([
+                    "status" => false,
+                    "message" => "Ops, task could not be updated."
+                ], 500);
+            }
+        }
 
     }
 
@@ -89,5 +154,33 @@ class AppointmentController extends Controller
      */
     public function destroy(Appointment $appointment)
     {
+        if ($appointment->delete()) {
+            return response()->json([
+                "status" => true,
+                "appointment" => $appointment
+            ]);
+        } else {
+            return response()->json([
+                "status" => false,
+                "message" => "Ops, task could not be deleted."
+            ]);
+        }
+    }
+
+
+    /* Special Methods */
+
+    // Randevu tarihlerinin kontrolü
+    public function appointmentDateControl($request){
+        $existStartDateCount = $this->user->appointments()->where('startdate', '<=', $request->startdate)->orderBy('startdate', 'DESC')->take(1)->where('enddate', '>=', $request->startdate)->get()->count();
+        $existEndDateCount = $this->user->appointments()->where('startdate', '<=', $request->enddate)->orderBy('startdate', 'DESC')->take(1)->where('enddate', '>=', $request->enddate)->get()->count();
+        $existStartAndEndDateCount = $this->user->appointments()->where('startdate', '>=', $request->startdate)->orderBy('startdate', 'ASC')->take(1)->where('startdate', '<=', $request->enddate)->get()->count();
+        if($existStartDateCount > 0 || $existEndDateCount > 0 || $existStartAndEndDateCount > 0)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
