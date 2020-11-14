0fce79921725df072db9e60dfc4418d605a86928
Özgür <ozgurumkii@gmail.com>
2020-11-14 11:16:38 +0300

    distance calculation methods were developed
    distance calculation methods were developed

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -8,6 +8,19 @@ class MapController extends Controller
 {
     public function distance($postcode)
     {
-        // mesafe hesaplama yapılacak
+        $latlan = self::getcoords($postcode);
+        $url = "https://api.distancematrix.ai/maps/api/distancematrix/json?&key=7U0LZhREnyqHOdFcwtAzhY02z4Fsr&origins=51.729157,0.478027&destinations=";
+        $url = $url.$latlan;
+        $data   = @file_get_contents($url);
+        $result = json_decode($data, true);
+        return $result;
+    }
+
+    public function getcoords($postcode){
+        $url = "http://api.postcodes.io/postcodes/".$postcode;
+        $data   = @file_get_contents($url);
+        $data = json_decode($data, true);
+        $result = $data['result'];
+        return $result['latitude'].','.$result['longitude'];
     }
 }
